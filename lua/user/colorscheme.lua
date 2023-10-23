6a759ca4416b9f1510dae903e740c918e173c9aa
th1rt3en <giuseppe.richetti@gmail.com>
2023-10-23 22:53:51 +0200

    Remove transparent background in colorscheme.lua

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,5 +1,7 @@
 -- Sets default colorscheme
 vim.cmd[[colorscheme rose-pine]]
 
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- This sets transparent background
+-- 
+--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
