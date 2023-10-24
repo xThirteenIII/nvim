c5ec80e1a5d44b3edbb8c319357fd359754cf8b0
th1rt3en <giuseppe.richetti@gmail.com>
2023-10-24 17:29:35 +0200

    Add keymaps to navigate to more harpoon files

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -14,6 +14,9 @@ function M.config()
     vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
     vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
     vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
+    vim.keymap.set("n", "<leader>5", function() ui.nav_file(5) end)
+    vim.keymap.set("n", "<leader>6", function() ui.nav_file(6) end)
+    vim.keymap.set("n", "<leader>7", function() ui.nav_file(7) end)
 
     require("harpoon").setup{}
 end
