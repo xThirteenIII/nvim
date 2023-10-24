d459c074204bc9826fd90773f5529fc048ee275e
Jisaa <giuseppe.richetti@gmail.com>
2023-10-24 10:01:31 +0200

    Add vim keymap <leader>fr to plyginkeys.lua

--- lua/user/pluginkeys.lua
+++ lua/user/pluginkeys.lua
@@ -8,7 +8,7 @@ vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fs', function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") });
 		end)
---vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {})
 --vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 -- Undotree keymaps
