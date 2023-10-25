b2e311beb8f148dcb1b52ebef5bf1578994207a1
th1rt3en <giuseppe.richetti@gmail.com>
2023-10-25 10:17:11 +0200

    Add keymaps for diagnostic

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -5,5 +5,14 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
+-- Open diagnostic floating window
+-- "K" again to move cursor into window, "q" to quit
+vim.keymap.set("n", "K", vim.diagnostic.open_float)
+
+-- Move to next diagnostic
+vim.keymap.set("n", "<leader>n", vim.diagnostic.goto_next)
+-- Move to previous diagnostic
+vim.keymap.set("n", "<leader>N", vim.diagnostic.goto_prev)
+
 
 
