71ea991d8d4270696ac2b9557ba37cd077250869
th1rt3en <giuseppe.richetti@gmail.com>
2023-11-21 19:14:36 +0100

    Replace indent carachter with basic pipe

--- lua/plugins/indentline.lua
+++ lua/plugins/indentline.lua
@@ -6,6 +6,7 @@ function M.config()
 
     local highlight = {
         "RainbowViolet",
+        "RainbowIris",
     }
 
     local hooks = require "ibl.hooks"
@@ -13,13 +14,26 @@ function M.config()
     -- every time the colorscheme changes
     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
         vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+        vim.api.nvim_set_hl(0, "RainbowIris", { fg = "#5D3FD3" })
     end)
         require("ibl").setup({
             indent = {
-                highlight = highlight,
-                char = "▏",
+                -- indent color is Iris
+                highlight = highlight[2],
+                char = "|",
+                smart_indent_cap = true,
+                priority = 2,
+            },
+
+            --  The scope is *not* the current indentation level! Instead, it is the
+            -- indentation level where variables or functions are accessible. This depends
+            -- on the language you are writing.
+            scope = {
+                -- active scope color is Violet
+                highlight = highlight[1],
             }
         })
 end
+
 
 return M
